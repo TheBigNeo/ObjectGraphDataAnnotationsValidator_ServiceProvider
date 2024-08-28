332b7ac50201309c97a2586a6dd2ef627017e1da
TheBigNeo <alexandro.will@outlook.com>
2024-08-28 17:01:02 +0200

    Add initial project structure with basic pages and settings
    Created essential components for the project, including the Home, Error, and App pages. Added configuration files such as appsettings.json and default styles. Initialized routes and imports for basic navigation and functionality.

---
+++ ObjectGraphDataAnnotationsValidator_ServiceProvider/Components/Sample/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObjectGraphDataAnnotationsValidator_ServiceProvider.Components.Sample;
+
+public class Animal : IValidatableObject
+{
+    public string Name { get; set; }
+
+    [Required] [ValidateComplexType] public List<Friend> Friends { get; set; } = [];
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        Console.Out.WriteLine("====> Validate Animal");
+
+        return new List<ValidationResult>();
+    }
+}
+
+public class Friend : IValidatableObject
+{
+    [Required] public string Name { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        Console.Out.WriteLine("====> Validate Complex Friend");
+
+        var animalService = validationContext.GetRequiredService<IAnimalService>();
+        animalService.MakeSound();
+
+        return new List<ValidationResult>();
+    }
+}
---
+++ ObjectGraphDataAnnotationsValidator_ServiceProvider/Components/Sample/AnimalService.cs
@@ -0,0 +1,14 @@
+﻿namespace ObjectGraphDataAnnotationsValidator_ServiceProvider.Components.Sample;
+
+public interface IAnimalService
+{
+    public void MakeSound();
+}
+
+public class AnimalService : IAnimalService
+{
+    public void MakeSound()
+    {
+        Console.Out.WriteLine("WUFF");
+    }
+}
